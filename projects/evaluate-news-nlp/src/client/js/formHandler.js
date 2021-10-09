d39d4864888a41a788f2a00383e29e2bde9e74fb
Kummari, Venky (NIH/OD) [C] <venky.kummari@nih.gov>
2021-10-09 16:50:06 -0400

    Initial version with key on the client side

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,16 +1,62 @@
-function handleSubmit(event) {
+export function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
-}
 
-export { handleSubmit }
+    const formdata = new FormData();
+    formdata.append("key", "54d3d886e269d58d09945623e4e88bc6");
+    formdata.append("txt", formText);
+    formdata.append("lang", "en");
+    formdata.append("of", "json");
+
+    const requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+    };
+
+    const response = fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
+        .then(response => response.body)
+        .then((res) => {
+            console.log(res)
+            const reader = res.getReader();
+
+            return new ReadableStream({
+                start(controller) {
+                    // The following function handles each data chunk
+                    function push() {
+                        // "done" is a Boolean and value a "Uint8Array"
+                        reader.read().then(({ done, value }) => {
+                            // If there is no more data to read
+                            if (done) {
+                                console.log('done', done);
+                                controller.close();
+                                return;
+                            }
+                            // Get the data and send it to the browser via the controller
+                            controller.enqueue(value);
+                            // Check chunks by logging to the console
+                            console.log(done, value);
+                            push();
+                        })
+                    }
+
+                    push();
+                }
+            });
+        })
+        .then(stream => {
+            // Respond with our stream
+            return new Response(stream, { headers: { "Content-Type": "text/html" } }).text();
+        })
+        .then(result => {
+            // Do things with result
+            console.log(result);
+            document.getElementById('results').innerHTML = result;
+        })
+        .catch(error => console.log('error', error));
+}
--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,6 +1,9 @@
 var path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const mockAPIResponse = require('./mockAPI.js')
+const meaningCloudApiKey = "54d3d886e269d58d09945623e4e88bc6"
 
 const app = express()
 
@@ -8,16 +11,22 @@ app.use(express.static('dist'))
 
 console.log(__dirname)
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function() {
+    console.log('Example app listening on port 8081!')
 })
 
-app.get('/test', function (req, res) {
+app.get('/test', function(req, res) {
     res.send(mockAPIResponse)
 })
+
+// Post route to post the Weather information
+app.get('/callMeaningCloud', (req, res) => {
+    Object.assign(projectData, req.body);
+    res.send(projectData);
+});
